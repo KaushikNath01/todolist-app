f7b75e76bffa190de32e2659dc7154d39e24db7c
Kaushik Nath <kaush.kn.735@gmail.com>
2020-12-17 23:59:16 +0530

    onclick prints input value

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const todo = document.querySelector('.add-todo');
+
+
+btn.addEventListener("click", addTodoList);
+todo.addEventListener("click", removeAddTodo);
+
+
+function addTodoList (e) {
+    e.preventDefault();
+    const createDiv = document.createElement('div');
+    createDiv.classList.add("create-div");
+    const createLi = document.createElement('li');
+    createLi.classList.add("create-li");
+    createLi.innerHTML = input.value;
+    createDiv.appendChild(createLi);
+    const createBtnOne = document.createElement('button');
+    createBtnOne.classList.add('btn-one');
+    createBtnOne.innerHTML = '+';
+    createDiv.appendChild(createBtnOne);
+    const createBtnTwo = document.createElement('button');
+    createBtnTwo.classList.add('btn-two');
+    createBtnTwo.innerHTML = '-';
+    createDiv.appendChild(createBtnTwo);
+    todo.appendChild(createDiv);
+    input.value = "";
+}
+
+
